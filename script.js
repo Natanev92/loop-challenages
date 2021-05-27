dd8a2eadcc2ef15e747ed21df389a50d127cd431
Natanev92 <nvillasenor1@icloud.com>
2021-05-27 14:45:04 -0700

    loops challanges

---
+++ script.js
@@ -0,0 +1,27 @@
+var x = 1
+for(var x; x < 20; x++){
+    console.log(x)
+}
+
+var x = 100
+for(var x; x>0; x-=3){
+    console.log(x)
+}
+
+var x = 4
+for(var x; x> -3.5; x-=1.5){
+    console.log(x)
+}
+
+
+var sum = 0;
+for(var x = 1; x <= 100; x++){
+        sum += x;
+        console.log(sum)
+    }
+
+    var product = 1
+for(var x=1; x<13; x++){
+    product *= x
+    console.log(product)
+}
